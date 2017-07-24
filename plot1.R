be6f3a9aebe751933f9793de5f92da1383ce5501
MANISH REDDY <jannepallymanishreddy@gmail.com>
2017-07-24 10:18:52 +0530

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,3 +1,14 @@
+rm(list = ls())
+setwd("C:/Users/janne/Desktop/R - Coursera/EDA- Project1")
+unzip("household_power_consumption.zip")
+data <- read.table("household_power_consumption.txt", header = T, 
+                   sep = ";", na.strings = "?")
+data$Date = as.Date(data$Date,"%d/%m/%Y")
+data$datetime <- strptime(paste(data$Date, data$Time), "%Y-%m-%d %H:%M:%S")
+req_data = subset(data,Date >= "2007-02-01" & Date <= "2007-02-02")
+
+
+
 par(mfrow = c(1,1), mar = c(4,4,2,2))
 png("plot1.png", width = 480, height = 480)
 dev.cur()
