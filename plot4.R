f1e99454c3997e8ecc19f928aef203cc3bc1c45b
MANISH REDDY <jannepallymanishreddy@gmail.com>
2017-07-24 10:20:32 +0530

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -1,3 +1,13 @@
+rm(list = ls())
+setwd("C:/Users/janne/Desktop/R - Coursera/EDA- Project1")
+unzip("household_power_consumption.zip")
+data <- read.table("household_power_consumption.txt", header = T, 
+                   sep = ";", na.strings = "?")
+data$Date = as.Date(data$Date,"%d/%m/%Y")
+data$datetime <- strptime(paste(data$Date, data$Time), "%Y-%m-%d %H:%M:%S")
+req_data = subset(data,Date >= "2007-02-01" & Date <= "2007-02-02")
+
+
 Globalactivepower = as.numeric(req_data$Global_active_power)
 Globalreactivepower = as.numeric(req_data$Global_reactive_power)
 Voltage = as.numeric(req_data$Voltage)
